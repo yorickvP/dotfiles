47fa0f90ed7c44e21397a0dc8311f3929fa61dea
Yorick van Pelt <yorickvanpelt@gmail.com>
2013-01-29 17:44:41 +0100

    remove menu bar from awesome and remove unused ugly thing

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -15,7 +15,6 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 -- Notification library
 local naughty = require("naughty")
-local menubar = require("menubar")
 -- widget library
 local vicious = require("vicious")
 
@@ -143,45 +142,10 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 awful.menu.menu_keys = { up    = { "k", "Up" },
                          down  = { "j", "Down" },
                          exec  = { "l", "Return", "Right" },
-                         -- the new item
                          enter = { "Right" },
-                         --
                          back  = { "h", "Left" },
                          close = { "q", "Escape" },
                        }
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
-background_timers = {}
-
-function run_background(cmd,funtocall)
-   local r = io.popen("mktemp")
-   local logfile = r:read("*line")
-   r:close()
-
-   cmdstr = cmd .. " &> " .. logfile .. " & "
-   local cmdf = io.popen(cmdstr)
-   cmdf:close()
-   background_timers[cmd] = {
-       file  = logfile,
-       timer = timer{timeout=1}
-   }
-   background_timers[cmd].timer:connect_signal("timeout",function()
-       local cmdf = io.popen("pgrep -f '" .. cmd .. "'")
-       local s = cmdf:read("*all")
-       cmdf:close()
-       if (s=="") then
-           background_timers[cmd].timer:stop()
-           local lf = io.open(background_timers[cmd].file)
-           funtocall(lf:read("*all"))
-           lf:close()
-           io.popen("rm " .. background_timers[cmd].file)
-       end
-   end)
-   background_timers[cmd].timer:start()
-end
 
 -- {{{ Wibox
 
@@ -526,8 +490,6 @@ globalkeys = awful.util.table.join(
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
               end),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end),
     -- locking
     awful.key({ modkey, "Control", "Shift" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end),
     -- brightness adjustment
