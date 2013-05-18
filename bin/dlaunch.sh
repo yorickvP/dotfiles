1b212604e962bc5c2094a1fc611cfb1c0dd1d63d
Yorick van Pelt <yorickvanpelt@gmail.com>
2013-05-18 18:59:58 +0200

    use yeganesh for launching things, fixes #1

--- bin/dlaunch.sh
+++ bin/dlaunch.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec dmenu_run -fn "-*-termsyn-medium-*-*-*-12-*-*-*-*-*-*-*" -p "run: " -i -f -nb "#1D1E24" -nf "#8DA893" -sb "#1D1E24" -sf "#C18E44" $@
+(
+ yeganesh -x -- -fn "-*-termsyn-medium-*-*-*-12-*-*-*-*-*-*-*" -p "run: " -i -f -nb "#1D1E24" -nf "#8DA893" -sb "#1D1E24" -sf "#C18E44" $@
+) | ${SHELL:-"/bin/sh"} &
