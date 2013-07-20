358dde5a152199559a8eaacf319bed7379368ba8
Yorick van Pelt <yorickvanpelt@gmail.com>
2013-07-20 17:16:37 +0200

    change terminal to termite and add config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init(confFolder .. "/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvtc"
+terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
