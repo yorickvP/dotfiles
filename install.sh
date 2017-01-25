e61a05c5c37edc26b027a8c75eea43a093f8a869
Yorick van Pelt <yorick@yorickvanpelt.nl>
2017-01-25 17:55:40 +0100

    add streamlinkrc

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p stow
-stow -d `dirname $0` -t ~ nix git x pentadactyl i3 gtk gpg mutt
+stow -d `dirname $0` -t ~ nix git x pentadactyl i3 gtk gpg mutt misc
 nix-build -A $(hostname -s)
