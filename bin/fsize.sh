6085c51881704f5c9919b2bde9935eb8a79c9972
Yorick van Pelt <yorick@yorickvanpelt.nl>
2017-01-25 12:38:21 +0100

    remove unused scripts

--- bin/fsize.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-printf '\33]50;%s%d%s\007' "xft:SourceCodePro:pixelsize=" $1 ":antialias=true:hinting=true"
-
--- bin/vga_setup.sh
+++
@@ -1,2 +0,0 @@
-xrandr --output VGA-0 --preferred --left-of LVDS-0 --output LVDS-0 --primary
-echo "power on" | sudo bluetoothctl
