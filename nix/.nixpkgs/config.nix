36c6abed07c81c48ee01a53e40de9bfde2c4e0fa
Yorick van Pelt <yorick@yorickvanpelt.nl>
2017-01-29 18:08:13 +0100

    git fire

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -45,6 +45,18 @@
       });
     };
 
+    gitFire = stdenv.mkDerivation {
+      src = fetchFromGitHub {
+        owner = "qw3rtman"; repo = "git-fire"; rev = "f485fffedbc4f719c55547be22ccd0080e592c9a";
+        sha256 = "1f7m1rypir85p33bgaggw5kgxjv7qy69zn1ci3b26rxr2a06fqwy";
+      };
+      name = "git-fire";
+      installPhase = ''
+        mkdir -p $out/bin/
+        install git-fire $out/bin/
+      '';
+    };
+
     weiightminder = pkgs.callPackage (fetchgit {
       url = https://gist.github.com/yorickvP/229d21a7da13c9c514dbd26147822641;
       rev = "9749ef4d83c0078bc0248215ee882d7124827cf3";
@@ -166,7 +178,7 @@
       ];
 
       code = mkEnv "y-code" [
-        python gitAndTools.hub gnumake cloc silver-searcher
+        python gitAndTools.hub gnumake cloc silver-searcher gitFire
       ];
       java = openjdk;
 
