f20a255bcec0e7a9406ec7283b657c04831f351e
Yorick van Pelt <yorick@yorickvanpelt.nl>
2017-02-01 19:29:08 +0100

    make the releases attrsets instead of lists

--- release-stable.nix
+++ release-stable.nix
@@ -1,7 +1,12 @@
 let pkgs = import <nixpkgs> {config = import ./nix/.nixpkgs/config.nix;};
+all_userspace = pkgs.lib.mapAttrs (name: paths: pkgs.buildEnv {inherit name paths;}) pkgs.hosts;
 in
 with pkgs;
 {
-	dotfiles = with (import ./default.nix pkgs); [woodhouse pennyworth frumar];
-	userspace = with (pkgs.lib.mapAttrs (name: paths: pkgs.buildEnv {inherit name paths;}) pkgs.hosts); [woodhouse pennyworth frumar];
+	dotfiles = {
+		inherit (import ./default.nix pkgs) woodhouse pennyworth frumar;
+	};
+	userspace = {
+		inherit (all_userspace) woodhouse pennyworth frumar;
+	};
 }
--- release-unstable.nix
+++ release-unstable.nix
@@ -1,7 +1,12 @@
 let pkgs = import <nixpkgs> {config = import ./nix/.nixpkgs/config.nix;};
+all_userspace = pkgs.lib.mapAttrs (name: paths: pkgs.buildEnv {inherit name paths;}) pkgs.hosts;
 in
 with pkgs;
 {
-	dotfiles = with (import ./default.nix pkgs); [ascanius jarvis];
-	userspace = with (pkgs.lib.mapAttrs (name: paths: pkgs.buildEnv {inherit name paths;}) pkgs.hosts); [ascanius jarvis];
+	dotfiles = {
+		inherit (import ./default.nix pkgs) ascanius jarvis;
+	};
+	userspace = {
+		inherit (all_userspace) ascanius jarvis;
+	};
 }
