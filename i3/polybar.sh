3853ff7e49ddb9ab4f383251185ec335006de5ae
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-03-29 12:31:01 +0200

    update polybar config

--- i3/polybar.sh
+++ i3/polybar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for m in $(xrandr --listactivemonitors | grep ": +" | cut -d " " -f 3 -); do
-    MONITOR=$(echo $m | tr -d +*) polybar -c ~/dotfiles/i3/polybar $(hostname)_$(echo $m | grep -q "*" && echo primary || echo other) &
+    MONITOR=$(echo $m | tr -d +*) polybar -c ~/dotfiles/i3/polybar $(hostname -s)_$(echo $m | grep -q "*" && echo primary || echo other) &
 done
