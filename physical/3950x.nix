55a85024bc2a52743fe7a071ba17dacee41719e9
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-01-02 20:50:12 +0100

    blackadder: pin linux 5.9, enable bluetooth

--- physical/3950x.nix
+++ physical/3950x.nix
@@ -12,10 +12,11 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "zfs" ];
   boot.kernelModules = [ "nct6775" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_5_9;
   networking.hostId = "c7736638";
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
+  hardware.bluetooth.enable = true;
 
   networking.useDHCP = false;
   networking.interfaces.enp9s0.useDHCP = true;
