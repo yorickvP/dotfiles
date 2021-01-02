86e5433e94603902e8df584e271c9e85e244b5e3
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-01-02 20:48:45 +0100

    server.nix: noXlibs workaround

--- roles/server.nix
+++ roles/server.nix
@@ -7,5 +7,10 @@
 
   environment.noXlibs = true;
   networking.firewall.logRefusedConnections = false; # Silence logging of scanners and knockers
-
+  # TODO: upstream with noXlibs
+  # https://github.com/NixOS/nixpkgs/pull/107394
+  nixpkgs.overlays = [ (self: super: {
+    elixir_1_8 = (self.beam.packagesWith (self.beam.interpreters.erlang_nox)).elixir_1_8;
+    erlang = super.erlang_nox;
+  }) ];
 }
