c2fbcb0896075749c18c0d1436f0889f01264cc5
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-01-03 17:40:19 +0100

    blackaddres: remove usb passthrough

--- logical/blackadder.nix
+++ logical/blackadder.nix
@@ -11,24 +11,6 @@
     name = "yorick-homepc";
     mtu = 1408;
   };
-  environment.systemPackages = [ pkgs.spice_gtk ];
-  security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice_gtk}/bin/spice-client-glib-usb-acl-helper";
-  virtualisation.virtualbox.host.enable = lib.mkForce true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.users.yorick.extraGroups = [ "vboxusers" ];
-
-  # users.users.pie = {
-  #   isNormalUser = true;
-  #   openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDpj2GrPpXtAp9Is0wDyQNl8EQnBiITkSAjhf7EjIqX" ];
-  # };
-  # services.nfs.server = {
-  #   enable = true;
-  #   exports = ''
-  #     /export 10.40.0.0/24(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0,no_root_squash)
-  #     /export/nfs/client1 10.40.0.0/24(insecure,rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=0,anongid=0,no_root_squash)
-  #     /export/nfs/client1/nix 10.40.0.0/24(insecure,ro,sync,no_subtree_check,crossmnt)
-  #   '';
-  # };
 
   services.znapzend = {
     enable = true;
