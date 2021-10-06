976d9808b1db4c7ccd020505a9e8f2ac35285ece
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-10-06 18:49:20 +0200

    fix pyroscope for now

--- pkgs/pyroscope/default.nix
+++ pkgs/pyroscope/default.nix
@@ -1,27 +1,43 @@
-{ fetchurl, fetchFromGitHub, python2Packages }:
-with python2Packages;
+{ fetchurl, fetchFromGitHub, python2 }:
 let
-	ProxyTypes = buildPythonPackage rec {
-	  pname = "ProxyTypes";
-    version = "0.10.0";
-    src = fetchPypi {
-      inherit pname version;
-	    sha256 = "11cr6c39vq9fky4c4h2ai2v2dva8fk4cfhaja0mrh4y9wzal3k42";
+  python2Packages = (python2.override {
+    packageOverrides = self: super: {
+      platformdirs = with self; (super.buildPythonPackage rec {
+        pname = "platformdirs";
+        version = "2.0.2";
+        src = fetchFromGitHub {
+          owner = pname;
+          repo = pname;
+          rev = version;
+          sha256 = "04gjhd6msdbqq8xm7x2vwlgldrxvvrvi86yq7g81dxlpzcwdyay8";
+        };
+        SETUPTOOLS_SCM_PRETEND_VERSION = version;
+        nativeBuildInputs = [ setuptools-scm ];
+        doCheck = false;
+      });
+	    ProxyTypes = super.buildPythonPackage rec {
+	      pname = "ProxyTypes";
+        version = "0.10.0";
+        src = self.fetchPypi {
+          inherit pname version;
+	        sha256 = "11cr6c39vq9fky4c4h2ai2v2dva8fk4cfhaja0mrh4y9wzal3k42";
+        };
+	    };
+	    pyrobase = super.buildPythonApplication rec {
+        pname = "pyrobase";
+        version = "0.5.2";
+	      src = fetchFromGitHub {
+  	      repo = pname; owner = "pyroscope";
+          rev = "v${version}";
+  	      sha256 = "170lsls3dmhlfa5abk40l365pk8486w48vkxjgs3pnqnhpp67z18";
+	      };
+	      doCheck = false;
+	      buildInputs = with self;[six paver];
+	    };
     };
-	};
-	pyrobase = buildPythonApplication rec {
-    pname = "pyrobase";
-    version = "0.5.2";
-	  src = fetchFromGitHub {
-  	  repo = pname; owner = "pyroscope";
-      rev = "v${version}";
-  	  sha256 = "170lsls3dmhlfa5abk40l365pk8486w48vkxjgs3pnqnhpp67z18";
-	  };
-	  doCheck = false;
-	  buildInputs = [six paver];
-	};
-
+  }).pkgs;
 in
+with python2Packages;
 buildPythonApplication rec {
   pname = "pyrocore";
   version = "0.5.3";
@@ -30,6 +46,7 @@ buildPythonApplication rec {
     rev = "v${version}";
   	sha256 = "0yg11nhrx8jzx8g09npf0pcpiscyh35nahhw473mir418plji5jw";
   };
+  doCheck = false; # hmm
   buildInputs = [paver];
-  propagatedBuildInputs = [six pyrobase ProxyTypes];
+  propagatedBuildInputs = [six pyrobase ProxyTypes setuptools];
 }
