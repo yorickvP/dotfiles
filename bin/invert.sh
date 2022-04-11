3dda3c774af393cfd15328488d995c49f6b99cc3
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-04-11 12:28:57 +0200

    emacs: atomically change colorscheme

--- bin/invert.sh
+++ bin/invert.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sed -i 's/colors:/colors-alt:/;t;s/colors-alt:/colors:/' ~/.config/alacritty/alacritty.yml
-(grep -B1 '^colors:$' ~/.config/alacritty/alacritty.yml | head -n1 | grep -qi light && echo "light" || echo dark) | tee ~/dotfiles/color-scheme
+(grep -B1 '^colors:$' ~/.config/alacritty/alacritty.yml | head -n1 | grep -qi light && echo "light" || echo dark) | tee ~/dotfiles/color-scheme.tmp
+mv ~/dotfiles/color-scheme.tmp ~/dotfiles/color-scheme
 #emacsclient -e "(set-solarized-theme nil '$(cat ~/dotfiles/color-scheme))" > /dev/null || true
 if [ "$(cat ~/dotfiles/color-scheme)" = "dark" ]; then
     dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
