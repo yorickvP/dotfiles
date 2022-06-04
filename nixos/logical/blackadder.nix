b8d487462501070b8dc9ddd7e26ca24602dfe076
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-06-04 14:33:32 +0200

    blackadder: fix backups

--- nixos/logical/blackadder.nix
+++ nixos/logical/blackadder.nix
@@ -21,7 +21,7 @@
       "rpool/home-enc" = {
         plan = "1d=>1h,1m=>1w";
         destinations.frumar = {
-          host = "root@192.168.178.37";
+          host = "root@192.168.178.110";
           dataset = "frumar-new/backup/blackadder";
           plan = "1w=>1d,1y=>1w,10y=>1m,50y=>1y";
         };
