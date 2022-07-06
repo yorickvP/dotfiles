e3e303ba0ae86da8f434be4e7d4e87322dd9a468
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-07-06 22:10:53 +0200

    blackadder: update backup addr

--- nixos/logical/blackadder.nix
+++ nixos/logical/blackadder.nix
@@ -21,7 +21,7 @@
       "rpool/home-enc" = {
         plan = "1d=>1h,1m=>1w";
         destinations.frumar = {
-          host = "root@192.168.178.110";
+          host = "root@frumar.home.yori.cc";
           dataset = "frumar-new/backup/blackadder";
           plan = "1w=>1d,1y=>1w,10y=>1m,50y=>1y";
         };
