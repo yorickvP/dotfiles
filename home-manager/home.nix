63a9a72ade83bfbbfd987189a24debfcc72f10e7
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-02-18 22:43:50 +0100

    git: globally ignore direnv and friends

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -37,6 +37,11 @@ in {
       extraConfig.help.autocorrect = 5;
       extraConfig.push.default = "simple";
       extraConfig.pull.ff = "only";
+      ignores = [
+        "/.envrc"
+        "/.cache"
+        "/.direnv"
+      ];
       aliases = {
         lg =
           "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
