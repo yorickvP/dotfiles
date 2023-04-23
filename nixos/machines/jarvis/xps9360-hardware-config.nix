a8ee5359f2e8d961b13e2a9bcceabe550bd699eb
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-04-23 12:25:48 +0200

    update laptops

--- nixos/machines/jarvis/xps9360-hardware-config.nix
+++ nixos/machines/jarvis/xps9360-hardware-config.nix
@@ -27,6 +27,4 @@
 
   nix.settings.max-jobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }
