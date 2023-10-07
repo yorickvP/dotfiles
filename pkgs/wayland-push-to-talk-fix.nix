220befc4a56d4736a69e16ebc98deef5ab14387b
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-10-07 19:14:57 +0200

    wayland-push-to-talk-fix: update to upstream

--- pkgs/wayland-push-to-talk-fix.nix
+++ pkgs/wayland-push-to-talk-fix.nix
@@ -3,10 +3,10 @@ stdenv.mkDerivation {
   pname = "wayland-push-to-talk-fix";
   version = "0.1";
   src = fetchFromGitHub {
-    owner = "yorickvP";
+    owner = "Rush";
     repo = "wayland-push-to-talk-fix";
-    rev = "57131c6983b083bb4677df9a073e66b84825b256";
-    hash = "sha256-46wk4sJTqNIU01wWnlcPU5rmzZHRcgUzVnYYJy95L7A=";
+    rev = "76c3227ede4b9e5881521826f47e396556eb7a63";
+    hash = "sha256-Wkt0MrCJH2IYvlewGUin49flM7bFlHpcqOkK7uks6Ho=";
   };
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ xorg.libX11 xorg.xorgproto xdotool libevdev ];
