26cbd9d4689f55c6a45f03c056d2cc73bf3e59da
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-02-17 10:46:50 +0100

    bump nats to 2.10.11

--- fixups.nix
+++ fixups.nix
@@ -2,14 +2,14 @@
   # https://github.com/NixOS/nixpkgs/pull/278153
   nats-server = super.buildGoModule rec {
     pname = "nats-server";
-    version = "2.10.7";
+    version = "2.10.11";
     src = pkgs.fetchFromGitHub {
       owner = "nats-io";
       repo = pname;
       rev = "v${version}";
-      hash = "sha256-DZ0a4gptTjuSVBlhDEWKTmU6Dgt36xulfjVK1kJtXhI=";
+      hash = "sha256-fRbjAqu1tFLUUk7aeIkEifcWkDUhNCbVZ957b2ntD+o=";
     };
     doCheck = false;
-    vendorHash = "sha256-Q2wc4esu2H81ct9TUPs+ysT3LrW698+9JllbvdDa5Yc=";
+    vendorHash = "sha256-lVCWTZvzLkYl+o+EUQ0kzIhgl9C236w9i3RRA5o+IAw=";
   };
 })
