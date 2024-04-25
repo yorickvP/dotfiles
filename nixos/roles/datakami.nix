b98abbf2afe61ed7db766c4ec86a13d667286e7e
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-04-25 10:26:23 +0200

    move dk-stage to datakami server

--- nixos/roles/datakami.nix
+++ nixos/roles/datakami.nix
@@ -1,34 +1,7 @@
 { lib, ... }: {
-  systemd.services.nginx.serviceConfig = {
-    ProtectHome = "tmpfs";
-    UMask = lib.mkForce "0022";
-    BindReadOnlyPaths = [ "/home/dk-stage/out" ];
-  };
-  systemd.tmpfiles.rules = [
-    "d /home/dk-stage 755 dk-stage dk-stage"
-    "d /home/dk-stage/out 755 dk-stage dk-stage"
-  ];
-  users.users.dk-stage = {
-    home = "/home/dk-stage";
-    group = "dk-stage";
-    useDefaultShell = true;
-    isSystemUser = true;
-    openssh.authorizedKeys.keys = with (import ../sshkeys.nix); [
-      ''command="rsync --server -logDtprcze.iLsfxCIvu --log-format=X --delete --partial . out/" ${dk-stage-deploy}''
-    ];
-    createHome = false; # sets wrong permissions
-  };
-  users.groups.dk-stage = { };
   services.nginx.virtualHosts."dk-stage.yori.cc" = {
     forceSSL = true;
     enableACME = true;
-    locations."/" = {
-      root = "/home/dk-stage/out";
-      index = "index.html";
-      extraConfig = ''
-        try_files $uri.html $uri $uri/ /index.html;
-        error_page 404 /404.html;
-      '';
-    };
+    globalRedirect = "staging.datakami.nl";
   };
 }
