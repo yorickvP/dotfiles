4979a04245ff7858cbaf78e42b3e135a3ef90db3
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-06-03 21:53:41 +0200

    dhcpcd: 10.0.6 -> 10.0.8

--- fixups.nix
+++ fixups.nix
@@ -1,3 +1,11 @@
 (pkgs: super: {
-
+  dhcpcd = super.dhcpcd.overrideAttrs (o: rec {
+    version = "10.0.8";
+    src = pkgs.fetchFromGitHub {
+      owner = "NetworkConfiguration";
+      repo = "dhcpcd";
+      rev = "v${version}";
+      sha256 = "sha256-kM+mdB7ul9NYHOEAJtp3M57M2MellrCoY/SaPWFLEpQ=";
+    };
+  });
 })
