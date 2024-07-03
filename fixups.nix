66f9fcff7ff02f24048bb99e51048481fed094ff
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-07-03 13:36:19 +0200

    patch openssh

--- fixups.nix
+++ fixups.nix
@@ -8,4 +8,11 @@
       sha256 = "sha256-kM+mdB7ul9NYHOEAJtp3M57M2MellrCoY/SaPWFLEpQ=";
     };
   });
+  openssh = super.openssh.overrideAttrs (o: rec {
+    version = "9.8p1";
+    src = pkgs.fetchurl {
+      url = "mirror://openbsd/OpenSSH/portable/openssh-${version}.tar.gz";
+      hash = "sha256-3YvQAqN5tdSZ37BQ3R+pr4Ap6ARh9LtsUjxJlz9aOfM=";
+    };
+  });
 })
