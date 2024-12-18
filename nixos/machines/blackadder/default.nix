3d02fa7e8d8a674e61119dc56967c9506f7a3372
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-12-18 11:01:55 +0100

    Remove mickey&bram

--- nixos/machines/blackadder/default.nix
+++ nixos/machines/blackadder/default.nix
@@ -42,14 +42,6 @@ in
       openssh.authorizedKeys.keys = sshkeys.lars;
     };
 
-    mickey = {
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = sshkeys.mickey ++ sshkeys.bram;
-      packages = with pkgs; [
-        git cmake gnumake gcc python3 python3.pkgs.pip screen vim r8-cog
-      ];
-      extraGroups = [ "docker" ];
-    };
     judith = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = sshkeys.judith;
