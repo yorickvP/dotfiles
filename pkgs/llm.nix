b9ba6e7e8f76504faa5baf492e5c559bdfcb836c
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-12-18 10:37:19 +0100

    Update LLM

--- pkgs/llm.nix
+++ pkgs/llm.nix
@@ -5,12 +5,12 @@ let
     imports = [ dream2nix.modules.dream2nix.pip ];
     name = "llm-env";
     deps.python = python3;
-    version = "0.17";
+    version = "0.19.1";
     pip.requirementsList = [
-      "llm==0.17.1"
-      "llm-claude-3==0.7"
+      "llm==0.19.1"
+      "llm-claude-3==0.10"
       "llm-replicate==0.3.1"
-      "llm-gemini==0.3"
+      "llm-gemini==0.7"
       "llm-cmd==0.2a0"
       "llm-jq==0.1.1"
       "llm-whisper-api==0.1.1"
