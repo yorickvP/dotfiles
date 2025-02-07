709ddc03dd42e81a4e351d86b1b0600f3a24baf9
Yorick van Pelt <yorick@yorickvanpelt.nl>
2025-02-07 10:09:10 +0100

    bump dream2nix, llm -> 0.21

--- pkgs/llm.nix
+++ pkgs/llm.nix
@@ -5,11 +5,11 @@ let
     imports = [ dream2nix.modules.dream2nix.pip ];
     name = "llm-env";
     deps.python = python3;
-    version = "0.20";
+    version = "0.21";
     pip.requirementsList = [
       "llm==0.21"
       "llm-anthropic==0.12"
-      "llm-gemini==0.9"
+      "llm-gemini==0.10"
       "llm-cmd==0.2a0"
       "llm-jq==0.1.1"
       "llm-whisper-api==0.1.1"
