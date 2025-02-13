db9923b1cf0e0d431e8f13997b338b6cdda995b8
Yorick van Pelt <yorick@yorickvanpelt.nl>
2025-02-13 11:44:25 +0100

    ghostty: use org.gtk.Applicaton.Activate call
    this avoids inheriting the CWD from window 1

--- bin/new-ghostty.sh
+++ bin/new-ghostty.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gdbus call --session --dest com.mitchellh.ghostty --object-path /com/mitchellh/ghostty/window/1 --method org.gtk.Actions.Activate 'new_window' [] {} || exec ghostty
+gdbus call --session --dest com.mitchellh.ghostty --object-path /com/mitchellh/ghostty --method org.gtk.Application.Activate '{}' || exec ghostty
