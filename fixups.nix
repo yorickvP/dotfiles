431833f5e7762ba429f1219b2119892fd457b153
Yorick van Pelt <yorick@yorickvanpelt.nl>
2025-05-05 17:32:27 +0200

    fixups for swaybg and bitwarden-desktop

--- fixups.nix
+++ fixups.nix
@@ -1,3 +1,19 @@
 (pkgs: super: {
   # ghostty is patched in flake.nix
+  swaybg = super.swaybg.override {
+    wrapGAppsNoGuiHook = null;
+  };
+  bitwarden-desktop = super.bitwarden-desktop.overrideAttrs (o: {
+    preBuild = ''
+      ${o.preBuild}
+      pushd apps/desktop/desktop_native/proxy
+      cargo build --bin desktop_proxy --release
+      popd
+    '';
+    postInstall = ''
+      ${o.postInstall or ""}
+      mkdir -p $out/bin
+      cp -r apps/desktop/desktop_native/target/release/desktop_proxy $out/bin
+    '';
+  });
 })
