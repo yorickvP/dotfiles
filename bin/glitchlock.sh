6fdc121765921cc46ffb76300a776b5b86a8ee08
Yorick van Pelt <yorick@yorickvanpelt.nl>
2025-05-23 17:52:06 +0200

    Attempt to improve glitchlock

--- bin/glitchlock.sh
+++ bin/glitchlock.sh
@@ -6,25 +6,26 @@ glitchedfile="/tmp/sclock_g.bmp"
 grim -t ppm $pngfile
 
 # convert to bmp and pixelate
-convert -scale 10% -scale 1000% $pngfile $bmpfile
+convert -scale 10% -scale 1000% $pngfile PPM:$bmpfile
 rm $pngfile
 
 # Glitch it with sox FROM: https://maryknize.com/blog/glitch_art_with_sox_imagemagick_and_vim/
 sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 100s : echo 0.9 0.9 15 0.9
 
 # Rotate it by 90 degrees
-convert -rotate 90 $glitchedfile $bmpfile
+convert $glitchedfile -rotate 90 PPM:$bmpfile
 
 #Glitch it again and rotate it back
 sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 90s : echo 0.9 0.9 15 1
-convert -rotate -90 $glitchedfile $glitchedfile
+convert $glitchedfile -rotate -90 PPM:$glitchedfile
 rm $bmpfile
 # Add lock icon, pixelate and convert back to png
 #  convert -gravity center -font "Hack-Bold-Nerd-Font-Complete-Mono" \
 #      -pointsize 200 -draw "text 0,240 ''" -channel RGBA -fill '#bf616a' \
 #      $glitchedfile $pngfile
 # convert $glitchedfile $pngfile
+convert $glitchedfile -crop 2560x1440+2560+0 PPM:/tmp/sclock_gr.bmp
 
-swaylock -i $glitchedfile
-#feh $pngfile
-rm $glitchedfile
+swaylock -i DVI-D-1:$glitchedfile -i DP-3:/tmp/sclock_gr.bmp
+#feh $glitchedfile
+rm $glitchedfile /tmp/sclock_gr.bmp
