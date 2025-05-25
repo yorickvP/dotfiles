296a18e396e86669da97d7966d2576440d105d80
Yorick van Pelt <yorick@yorickvanpelt.nl>
2025-05-25 09:30:11 +0200

    [blackadder] enable nvidia open driver

--- nixos/machines/blackadder/3950x.nix
+++ nixos/machines/blackadder/3950x.nix
@@ -24,7 +24,7 @@
   # };
   environment.systemPackages = [ pkgs.openrgb pkgs.egl-wayland ];
   services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
-  hardware.nvidia.open = false; # TODO
+  hardware.nvidia.open = true;
   hardware.nvidia.powerManagement.finegrained = true;
   hardware.nvidia.prime.offload.enable = true;
   hardware.nvidia.prime = {
