32e00d637e5fa6445e6f312c5c80baeff21b875b
Yorick van Pelt <yorick@yorickvanpelt.nl>
2025-12-07 18:20:36 +0100

    butterscotch: downgrade linux-firmware to fix amdgpu bug

--- nixos/machines/butterscotch/default.nix
+++ nixos/machines/butterscotch/default.nix
@@ -24,6 +24,18 @@
   networking.hostId = "d05ee74c";
 
   system.stateVersion = "25.05";
-  boot.kernelPackages = pkgs.linuxPackages_6_16;
+  boot.kernelPackages = pkgs.linuxPackages_6_17;
+  # temp
+  nixpkgs.overlays = [(self: super: {
+    linux-firmware = super.linux-firmware.overrideAttrs (rec {
+      version = "20251111";
+      src = self.fetchFromGitLab {
+        owner = "kernel-firmware";
+        repo = "linux-firmware";
+        tag = version;
+        hash = "sha256-YGcG2MxZ1kjfcCAl6GmNnRb0YI+tqeFzJG0ejnicXqY=";
+      };
+    });
+  })];
 
 }
