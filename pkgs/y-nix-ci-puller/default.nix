73fede48ac4c40fb760fd9f30ca9c53f74dfb14a
Yorick van Pelt <yorick@yorickvanpelt.nl>
2026-03-22 12:03:55 +0100

    Add y-nix-ci-apply

--- pkgs/y-nix-ci-puller/default.nix
+++ pkgs/y-nix-ci-puller/default.nix
@@ -1,4 +1,11 @@
-{ lib, buildGoModule }:
+{
+  lib,
+  buildGoModule,
+  installShellFiles,
+  makeWrapper,
+  nix,
+  systemd,
+}:
 
 buildGoModule rec {
   pname = "y-nix-ci-puller";
@@ -7,6 +14,22 @@ buildGoModule rec {
   src = ./.;
 
   vendorHash = "sha256-Db09ftEG9DJgN6mb4LaA2cOGiOjQx36DzeDqzAik2Fs=";
+
+  nativeBuildInputs = [
+    installShellFiles
+    makeWrapper
+  ];
+
+  postInstall = ''
+    install -Dm755 ${./y-nix-ci-apply} $out/bin/y-nix-ci-apply
+    wrapProgram $out/bin/y-nix-ci-apply \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          nix
+          systemd
+        ]
+      }
+  '';
 
   meta = with lib; {
     description = "Pull CI-built nix store paths via MQTT and create GC roots";
