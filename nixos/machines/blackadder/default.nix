40dc147a18a5cb1580472025de383dca0124b83a
Yorick van Pelt <yorick@yorickvanpelt.nl>
2026-04-11 10:37:13 +0200

    Restricted znapzend receiver
    Fixes #60

--- nixos/machines/blackadder/default.nix
+++ nixos/machines/blackadder/default.nix
@@ -21,7 +21,7 @@ in
       "rpool/home-enc" = {
         plan = "1d=>1h,1m=>1w";
         destinations.frumar = {
-          host = "root@frumar.home.yori.cc";
+          host = "znapzend-blackadder@frumar.home.yori.cc";
           dataset = "frumar-new/backup/blackadder";
           plan = "1w=>1d,1y=>1w,10y=>1m,50y=>1y";
         };
@@ -29,7 +29,7 @@ in
       "rpool/home-enc/judith" = {
         plan = "1d=>1h,1m=>1w";
         destinations.frumar = {
-          host = "root@frumar.home.yori.cc";
+          host = "znapzend-blackadder@frumar.home.yori.cc";
           dataset = "frumar-new/backup/blackadder/judith";
           plan = "1m=>1w";
         };
